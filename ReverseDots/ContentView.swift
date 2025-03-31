d58bc85ff0cf87bce2bff8c7c2de2cfe7dd5d097
jutamin <wonjy0307@naver.com>
2025-03-31 21:11:01 +0900

    Initial Commit

---
+++ ReverseDots/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReverseDots
+//
+//  Created by 원주연 on 3/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReverseDots/ReverseDotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReverseDotsApp.swift
+//  ReverseDots
+//
+//  Created by 원주연 on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct ReverseDotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
